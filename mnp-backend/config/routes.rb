9d616c517d9238936c5738e4fc29067061d16b79
kchohee <56492609+kchohee@users.noreply.github.com>
2020-07-17 00:35:56 -0500

    created sessions

--- mnp-backend/config/routes.rb
+++ mnp-backend/config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/destroy'
   namespace :api do
     namespace :v1 do
       resources :users
+      resources :sessions, only: [:new, :create, :destroy]
     end
   end
+  
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
