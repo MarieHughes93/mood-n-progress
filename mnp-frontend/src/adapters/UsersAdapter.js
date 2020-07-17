d98346a677c62219c95b8a4b042a48b2ba796700
kchohee <56492609+kchohee@users.noreply.github.com>
2020-07-16 22:10:43 -0500

    creat user fuctional

--- mnp-frontend/src/adapters/UsersAdapter.js
+++ mnp-frontend/src/adapters/UsersAdapter.js
@@ -9,4 +9,18 @@ class UsersAdapter {
         return fetch(this.baseUrl).then(res => res.json()
         )
     }
+    createUser(value) {
+        const user = {
+            name: value,
+            email: value,
+            password: value,
+        }
+        return fetch(this.baseUrl,{
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({user}),
+        }).then(res=> res.json())
+    }
 }
--- mnp-frontend/src/components/users.js
+++ mnp-frontend/src/components/users.js
@@ -7,13 +7,32 @@ class Users{
     }
     initBindingAndEventListners(){
         this.usersContainer = document.getElementById('users-container')
+        this.newUserName = document.getElementById('new-user-name')
+        this.newUserEmail = document.getElementById('new-user-email')
+        this.newUserPassword = document.getElementById('new-user-password')
+        this.userForm = document.getElementById('new-user-form')
+        this.userForm.addEventListener('submit', this.createUser.bind(this))
+    }
+
+    createUser(e){
+        e.preventDefault()
+        const name = this.newUserName.value
+        const email = this.newUserEmail.value
+        const password = this.newUserPassword.value
+        this.adapter.createUser(name,email).then(user=> {
+            this.users.push(new User(user))
+            this.newUserName.value = ''
+            this.newUserEmail.value = ''
+            this.newUserPassword.value = ''
+            this.render()
+        })
     }
 
     fetchAndLoadUsers(){
         this.adapter
         .getUsers()
         .then(users => {
-            users.forEach(user => this.users.push(new User(user)))
+            users.forEach(user => this.users.push(new User(user))) 
         })
         .then(()=>{
             this.render()
