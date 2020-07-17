a5da76837261fcf527cd029d374163d6343dc229
kchohee <56492609+kchohee@users.noreply.github.com>
2020-07-17 11:38:57 -0500

    created login method & login listner

--- mnp-frontend/src/components/app.js
+++ mnp-frontend/src/components/app.js
@@ -3,27 +3,28 @@ class App{
     constructor() {
         this.current_user = ''
         this.users = new Users()
+        this.loginListner()
+        this.adapter = new UsersAdapter
     }
 
     loginListner(){
         this.loginContainer = document.getElementById('login-container')
         this.loginEmail = document.getElementById('login-user-email')
         this.loginPassword = document.getElementById('login-user-password')
-        this.loginForm = document.getElementById('new-user-form')
+        this.loginForm = document.getElementById('login-form')
+        // this bind on 14 is for us to be speaking of the app instead of the login form.
         this.loginForm.addEventListener('submit', this.login.bind(this))
     }
     login(e){
         e.preventDefault()
         const email = this.loginEmail.value
-        const password = this.loginPassword.value
-        this.adapter.login(email,password).then(user=> {
-            this.current_user = user
+        this.adapter.login(email).then(user=> {
+            this.current_user = new User(user)
             this.newUserContainer = document.getElementById('new-user-container')
             this.loginContainer = document.getElementById('login-container')
             this.loginContainer.value = ''
             this.newUserContainer.value = ''
-            this.render()
-        }).bind(this)
+        })
     }
 
  
